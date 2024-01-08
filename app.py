8b82177bb6e8450ef84e6095d871618b45838c71
MohammadKhGh99 <m7md.gh.99@hotmail.com>
2024-01-09 00:36:56 +0200

    made some of the main page, need to add navigation bar

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import webbrowser
+
+from flask import render_template, Flask, request, redirect, url_for, flash
+
+app = Flask(__name__)
+app.secret_key = 'lakjfpoek[gf;sldg165478'
+app.config['DEBUG'] = True
+
+products = [filename for filename in os.listdir('static/images/products')] * 4
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', products=products)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    webbrowser.open("http://127.0.0.1:5000")
+    app.run()
